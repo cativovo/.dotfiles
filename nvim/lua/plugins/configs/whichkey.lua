19f7dd109b44eb3c57574eb1aa43303c335392ba
Mark Leo Cativo <leo.cativo@coherent.global>
2022-10-05 15:36:49 +0800

    add which-key enhancements

--- nvim/lua/plugins/configs/whichkey.lua
+++ nvim/lua/plugins/configs/whichkey.lua
@@ -54,8 +54,8 @@ local function setup()
         text_objects = false, -- help for text objects triggered after entering an operator
         windows = false, -- default bindings on <c-w>
         nav = false, -- misc bindings to work with windows
-        z = false, -- bindings for folds, spelling and others prefixed with z
-        g = false, -- bindings for prefixed with g
+        z = true, -- bindings for folds, spelling and others prefixed with z
+        g = true, -- bindings for prefixed with g
       },
       spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
     },
