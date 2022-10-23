0b405abf2601aad5c3acebc212c47b1978c155ea
Mark Leo Cativo <leo.cativo@coherent.global>
2022-10-23 11:16:37 +0800

    refactor autocmds

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -2,7 +2,44 @@ local M = {}
 local autocmd = vim.api.nvim_create_autocmd
 local create_augroup = vim.api.nvim_create_augroup
 
+M.augroups = {
+  format_on_save = create_augroup("LspFormatOnSave", {}),
+  toggle_number = create_augroup("ToggleNumber", {}),
+}
 
+M.events = {
+  format_on_save = "BufWritePre"
+}
+
+-- augroups
+M.format_on_save = function(bufnr, callback)
+  autocmd(M.events.format_on_save, {
+    group = M.augroups.format_on_save,
+    buffer = bufnr,
+    callback = function()
+      callback(bufnr)
+    end,
+  })
+end
+
+M.toggle_number = function()
+  autocmd("InsertLeave",
+    {
+      group = M.augroups.toggle_number,
+      pattern = "*.*",
+      desc = "Set to rnu",
+      command = "set rnu",
+    }
+  )
+  autocmd("InsertEnter",
+    {
+      group = M.augroups.toggle_number,
+      pattern = "*.*",
+      desc = "Set to nornu",
+      command = "set nornu",
+    }
+  )
+end
 
 M.load = function()
   -- general autocmds
@@ -16,44 +53,12 @@ M.load = function()
     }
   )
 
-  autocmd("InsertLeave",
-    {
-      pattern = "*.*",
-      desc = "Set to rnu",
-      command = "set rnu",
-    }
-  )
-
-  autocmd("InsertEnter",
-    {
-      pattern = "*",
-      desc = "Set to nornu",
-      command = "set nornu",
-    }
-  )
-
   autocmd({ "BufWinEnter", "BufRead", "BufNewFile" }, {
     pattern = "*",
     command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o"
   })
-end
 
-M.augroups = {
-  format_on_save = create_augroup("LspFormatOnSave", {})
-}
-
-M.events = {
-  format_on_save = "BufWritePre"
-}
-
-M.format_on_save = function(bufnr, callback)
-  vim.api.nvim_create_autocmd(M.events.format_on_save, {
-    group = M.augroups.format_on_save,
-    buffer = bufnr,
-    callback = function()
-      callback(bufnr)
-    end,
-  })
+  M.toggle_number()
 end
 
 return M
