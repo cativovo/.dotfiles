4e2d0cbcba5021694237e0f44b13669ebf68658c
Mark Leo Cativo <leo.cativo@coherent.global>
2023-03-28 20:55:31 +0800

    refactor telescope config

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -1,51 +1,50 @@
 local M = {}
 
-local themes = require("telescope.themes")
-local telescope_builtin = require("telescope.builtin")
-local telescope_actions = require("telescope.actions");
-
-local opts = {
-  defaults = {
-    file_ignore_patterns = { ".git" },
-    sorting_strategy = "ascending",
-    layout_strategy = "vertical",
-    layout_config = {
-      vertical = {
-        preview_cutoff = 0,
-        prompt_position = "top",
-        mirror = true,
-        preview_height = .70,
-      },
-      width = 0.90,
-      height = 0.99,
-    },
-    mappings = require("core.keymaps").telescope.get("setup", telescope_actions)
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,                   -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true,    -- override the file sorter
-      -- the default case_mode is "smart_case"
-      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-    },
-    ["ui-select"] = {
-      themes.get_cursor({
-        layout_config = {
-          width = 0.80,
-        },
-      })
-    }
-  },
-}
-
 M.setup = function()
-  local telescope = require("telescope")
-  telescope.setup(opts)
+	local themes = require("telescope.themes")
+	local telescope_actions = require("telescope.actions")
 
-  -- load extensions
-  telescope.load_extension("ui-select")
-  telescope.load_extension("fzf")
+	local opts = {
+		defaults = {
+			file_ignore_patterns = { ".git" },
+			sorting_strategy = "ascending",
+			layout_strategy = "vertical",
+			layout_config = {
+				vertical = {
+					preview_cutoff = 0,
+					prompt_position = "top",
+					mirror = true,
+					preview_height = 0.70,
+				},
+				width = 0.90,
+				height = 0.99,
+			},
+			mappings = require("core.keymaps").telescope.get("setup", telescope_actions),
+		},
+		extensions = {
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				-- the default case_mode is "smart_case"
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			},
+			["ui-select"] = {
+				themes.get_cursor({
+					layout_config = {
+						width = 0.80,
+					},
+				}),
+			},
+		},
+	}
+
+	local telescope = require("telescope")
+	telescope.setup(opts)
+
+	-- load extensions
+	telescope.load_extension("ui-select")
+	telescope.load_extension("fzf")
 end
 
 return M
