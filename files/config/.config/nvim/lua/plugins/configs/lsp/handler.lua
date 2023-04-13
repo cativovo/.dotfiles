239ebeed5b6967f339fa29bfd00a5f9058410931
Mark Leo Cativo <leo.cativo@coherent.global>
2023-04-13 23:04:02 +0800

    disable semanticTokensProvider

--- files/config/.config/nvim/lua/plugins/configs/lsp/handler.lua
+++ files/config/.config/nvim/lua/plugins/configs/lsp/handler.lua
@@ -4,7 +4,10 @@ local register_keys = function(buffer)
 	require("which-key").register(require("core.keymaps").lsp.normal, { buffer })
 end
 
-M.common_on_attach = function(_, buffer)
+M.common_on_attach = function(client, buffer)
+	-- disable custom sytax highlighting of each server
+	client.server_capabilities.semanticTokensProvider = nil
+
 	register_keys(buffer)
 	local autocmds = require("core.autocmds")
 	autocmds.autoformat(buffer)
