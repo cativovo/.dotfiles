cc59e0ad58e6e362f709e184791b7e22c01b7021
Mark Leo Cativo <leo.cativo@coherent.global>
2023-04-13 22:27:45 +0800

    allow adding of server specific on_attach

--- files/config/.config/nvim/lua/plugins/configs/lsp/handler.lua
+++ files/config/.config/nvim/lua/plugins/configs/lsp/handler.lua
@@ -4,7 +4,7 @@ local register_keys = function(buffer)
 	require("which-key").register(require("core.keymaps").lsp.normal, { buffer })
 end
 
-M.on_attach = function(_, buffer)
+M.common_on_attach = function(_, buffer)
 	register_keys(buffer)
 	local autocmds = require("core.autocmds")
 	autocmds.autoformat(buffer)
