336ad7097211c9fb7ac2c3ca51c2505550fe13b4
Mark Leo Cativo <leo.cativo@coherent.global>
2023-04-17 12:57:49 +0800

    add fzf

--- files/shell/.zsh/lib/functions.zsh
+++ files/shell/.zsh/lib/functions.zsh
@@ -5,3 +5,12 @@ cdx() {
     exa -a;
   fi
 }
+
+fzf-directory() {
+  local DIRECTORIES=$(fd . --type d --hidden --exclude '.git' | fzf)
+
+  if [[ -n "${DIRECTORIES}" ]]; then
+    cd $DIRECTORIES
+     zle reset-prompt
+  fi
+}
