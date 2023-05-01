8393e6e88da6e6312f1b35fccf726e2460f4cb9b
Mark Leo Cativo <leo.cativo@coherent.global>
2023-05-01 20:57:01 +0800

    unset load-nvm after using

--- files/shell/.zsh/lib/functions.zsh
+++ files/shell/.zsh/lib/functions.zsh
@@ -91,7 +91,7 @@ find-nvmrc() {
 
 # lazy load nvm
 load-nvm() {
-   unset -f nvm node yarn npm
+   unset -f nvm node yarn npm load-nvm
    echo "Loading nvm...";
    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";  # This loads nvm
    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion";  # This loads nvm bash_completion
