933bc86739885f4e60ac9412f8003be5b264b705
Mark Leo Cativo <leo.cativo@coherent.global>
2023-06-06 04:33:08 +0800

    update rust-analyzer server settings to use clippy instead of check

--- files/config/.config/nvim/lua/plugins/configs/lsp/server_options/rust_analyzer.lua
+++ files/config/.config/nvim/lua/plugins/configs/lsp/server_options/rust_analyzer.lua
@@ -2,4 +2,11 @@ return {
 	on_attach = function(_, buffer)
 		require("which-key").register(require("core.keymaps").rust.normal, { buffer })
 	end,
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = "clippy",
+			},
+		},
+	},
 }
