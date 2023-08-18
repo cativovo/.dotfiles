b55d9cae79826bef783dda1b5e694a83165e3e72
Mark Leo Cativo <leo.cativo@coherent.global>
2023-08-18 20:11:27 +0800

    nvim: fix jsx comment

--- files/config/.config/nvim/lua/plugins/configs/mini_comment.lua
+++ files/config/.config/nvim/lua/plugins/configs/mini_comment.lua
@@ -3,9 +3,9 @@ local M = {}
 M.setup = function()
 	local opts = {
 		mappings = require("core.keymaps").comment.setup,
-		hooks = {
-			pre = function()
-				require("ts_context_commentstring.internal").update_commentstring()
+		options = {
+			custom_commentstring = function()
+				return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
 			end,
 		},
 	}
