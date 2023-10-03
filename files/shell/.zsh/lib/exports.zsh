d18fb0e36a5d0202a272fb86509431c33bbecefe
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-10-03 21:54:56 +0800

    zsh: add go path

--- files/shell/.zsh/lib/exports.zsh
+++ files/shell/.zsh/lib/exports.zsh
@@ -5,7 +5,7 @@ export VISUAL="nvim"
 export EDITOR="$VISUAL"
 
 # add something here if needed
-export PATH="$PATH:$HOME/.mvn/bin"
+export PATH="$PATH:$HOME/.mvn/bin:$HOME/go/bin"
 
 # change default config path of starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
