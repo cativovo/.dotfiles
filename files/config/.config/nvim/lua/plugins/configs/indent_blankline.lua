f39f4c0899aa9b870f1e4a604b69338972aa084f
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-10-11 12:09:50 +0800

    nvim: fix indentline breaking changes

--- files/config/.config/nvim/lua/plugins/configs/indent_blankline.lua
+++ files/config/.config/nvim/lua/plugins/configs/indent_blankline.lua
@@ -2,14 +2,22 @@ local M = {}
 
 M.setup = function()
 	local opts = {
-		-- char = "▏",
-		char = "│",
-		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-		show_trailing_blankline_indent = false,
-		show_current_context = false,
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		exclude = {
+			filetypes = {
+				"help",
+				"dashboard",
+				"lazy",
+				"mason",
+			},
+		},
+		scope = { enabled = false },
 	}
 
-	require("indent_blankline").setup(opts)
+	require("ibl").setup(opts)
 end
 
 return M
