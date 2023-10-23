ad7b41ceed892615a7faf28b922c8a19164e61b4
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-10-23 17:39:03 +0800

    nvim: update cmp formatting

--- files/config/.config/nvim/lua/plugins/cmp.lua
+++ files/config/.config/nvim/lua/plugins/cmp.lua
@@ -8,6 +8,28 @@ return {
       ["<C-u>"] = cmp.mapping.scroll_docs(-4),
       ["<C-d>"] = cmp.mapping.scroll_docs(4),
     })
+
+    opts.formatting = vim.tbl_extend("force", opts.formatting, {
+      format = function(_, item)
+        local icons = require("lazyvim.config").icons.kinds
+        if icons[item.kind] then
+          item.kind = icons[item.kind] .. item.kind
+        end
+
+        local max_text_length = 27
+
+        if #item.abbr > max_text_length then
+          item.abbr = string.sub(item.abbr, 1, max_text_length - 1) .. "…"
+        end
+
+        if item.menu ~= nil then
+          item.menu = nil
+        end
+
+        return item
+      end,
+    })
+
     opts.window = vim.tbl_extend("force", opts.window or {}, {
       completion = bordered(),
       documentation = bordered(),
