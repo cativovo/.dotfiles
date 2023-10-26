b8fe03adb858960ce01842ec5d0217c3129b521f
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-10-26 15:27:33 +0800

    nvim: add keymap for commenting in visual mode

--- files/config/.config/nvim/lua/plugins/mini_comment.lua
+++ files/config/.config/nvim/lua/plugins/mini_comment.lua
@@ -5,9 +5,11 @@ return {
       -- macos
       comment = "<C-_>",
       comment_line = "<C-_>",
+      comment_visual = "<C-_>",
       -- linux
       -- comment = "<C-/>",
       -- comment_line = "<C-/>",
+      -- comment_visual = "<C-/>",
       textobject = "gc",
     },
   },
