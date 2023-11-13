a99de9fc65dd454d8bea548f5d1a8b1479419ee8
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-11-13 13:15:39 +0800

    nvim: add oil plugin

---
+++ files/config/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,33 @@
+return {
+  "stevearc/oil.nvim",
+  cmd = "Oil",
+  keys = {
+    {
+      "<leader>o",
+      function()
+        require("oil.actions").open_cwd.callback()
+      end,
+      desc = "Oil",
+    },
+  },
+  opts = {
+    -- https://github.com/stevearc/oil.nvim#options
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+      ["<C-p>"] = false,
+      ["<C-s>"] = false,
+      ["<C-t>"] = false,
+      ["<C-v>"] = "actions.select_vsplit",
+      ["<C-x>"] = "actions.select_split",
+      ["<leader>r"] = "actions.refresh",
+      ["<leader>y"] = "actions.copy_entry_path",
+    },
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
