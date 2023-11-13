8a92f1a0c2e5c22a9de4c3c91c6523ab04a1c9ab
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-11-13 23:16:49 +0800

    nvim: update lsp config

--- files/config/.config/nvim/lua/plugins/lsp.lua
+++ files/config/.config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- DISABLED
-    keys[#keys + 1] = { "]d", false }
-    keys[#keys + 1] = { "[d", false }
-    keys[#keys + 1] = { "]e", false }
-    keys[#keys + 1] = { "[e", false }
-    keys[#keys + 1] = { "]w", false }
-    keys[#keys + 1] = { "[w", false }
-    -- MY KEYS
-    keys[#keys + 1] = { "<A-l>", vim.diagnostic.goto_next, "Next Diagnostic" }
-    keys[#keys + 1] = { "<A-h>", vim.diagnostic.goto_prev, "Prev Diagnostic" }
-  end,
   opts = {
     diagnostics = {
       float = { border = "rounded" },
+      virtual_text = false,
     },
   },
 }
