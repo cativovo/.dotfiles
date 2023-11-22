51b6f55cc74fc2ff02956f8700f41f4f76d3487d
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-11-22 14:08:42 +0800

    nvim: move lazyvim extras to lazyvim.json

--- files/config/.config/nvim/lua/config/lazy.lua
+++ files/config/.config/nvim/lua/config/lazy.lua
@@ -11,23 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-    -- formatting
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-
-    -- lang
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-
-    -- lint
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-
     -- import/override with your plugins
     { import = "plugins" },
   },
