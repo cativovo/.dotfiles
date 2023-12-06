de21f93c5a9e14e62e7ecf34d8951275ffe099a3
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-12-06 10:25:29 +0800

    Revert "nvim: update harpoon"
    This reverts commit 073ea86f92b74fc08809e725135c0a85bf26b241.

--- files/config/.config/nvim/lua/plugins/harpoon.lua
+++ files/config/.config/nvim/lua/plugins/harpoon.lua
@@ -1,56 +1,61 @@
 return {
   "ThePrimeagen/harpoon",
   event = "VeryLazy",
-  branch = "harpoon2",
-  config = function(_, opts)
-    require("harpoon"):setup(opts)
-  end,
-  opts = {
-    settings = {
-      save_on_toggle = true,
-    },
-  },
+  config = true,
   keys = {
     {
       "<leader>fa",
       function()
-        require("harpoon"):list():append()
+        require("harpoon.mark").add_file()
       end,
       desc = "Add File to Harpoon",
     },
     {
       "<leader>fm",
       function()
-        local harpoon = require("harpoon")
-        harpoon.ui:toggle_quick_menu(harpoon:list())
+        require("harpoon.ui").toggle_quick_menu()
       end,
       desc = "Harpoon Menu",
     },
     {
+      "<leader>fn",
+      function()
+        require("harpoon.ui").nav_next()
+      end,
+      desc = "Next File",
+    },
+    {
+      "<leader>fp",
+      function()
+        require("harpoon.ui").nav_prev()
+      end,
+      desc = "Previous File",
+    },
+    {
       "<leader>fj",
       function()
-        require("harpoon"):list():select(1)
+        require("harpoon.ui").nav_file(1)
       end,
       desc = "First File",
     },
     {
       "<leader>fk",
       function()
-        require("harpoon"):list():select(2)
+        require("harpoon.ui").nav_file(2)
       end,
       desc = "Second File",
     },
     {
       "<leader>fl",
       function()
-        require("harpoon"):list():select(3)
+        require("harpoon.ui").nav_file(3)
       end,
       desc = "Third File",
     },
     {
       "<leader>f;",
       function()
-        require("harpoon"):list():select(4)
+        require("harpoon.ui").nav_file(4)
       end,
       desc = "Fourth File",
     },
