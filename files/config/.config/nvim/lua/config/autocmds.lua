4bad0f17483cde789e65ddf8d957944e48ed355f
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-12-11 15:36:08 +0800

    nvim: remove unused auto_cmd

--- files/config/.config/nvim/lua/config/autocmds.lua
+++ files/config/.config/nvim/lua/config/autocmds.lua
@@ -2,19 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local register_keys = function(source, pattern, desc)
-  local mappings = source.mappings
-  local mapping_opts = source.opts
-
-  vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = pattern,
-    desc = desc,
-    callback = function()
-      require("which-key").register(mappings, mapping_opts)
-    end,
-  })
-end
-
 -- Save Folds
 vim.api.nvim_create_autocmd("BufWinLeave", {
   pattern = "?*",
