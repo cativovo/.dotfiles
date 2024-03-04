f7e0cba97a75cc6ca3edb20d701590d3bad39bf8
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-03-04 10:59:45 +0800

    nvim: update treesitter context config

--- files/config/.config/nvim/lua/plugins/treesitter_context.lua
+++ files/config/.config/nvim/lua/plugins/treesitter_context.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
-  opts = function()
+  opts = {
+    max_lines = 0, -- <= 0 mean no limit
+  },
+  config = function(_, opts)
     vim.api.nvim_set_hl(0, "TreesitterContextBottom", { sp = "Grey", underline = true })
+    require("treesitter-context").setup(opts)
   end,
 }
