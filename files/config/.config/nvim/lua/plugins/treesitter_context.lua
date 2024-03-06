09f6c97061d1f1650136e7654f3131e7ea41260f
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-03-06 09:07:33 +0800

    nvim: change treesitter context max_lines

--- files/config/.config/nvim/lua/plugins/treesitter_context.lua
+++ files/config/.config/nvim/lua/plugins/treesitter_context.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   opts = {
-    max_lines = 0, -- <= 0 mean no limit
+    max_lines = 6,
   },
   config = function(_, opts)
     vim.api.nvim_set_hl(0, "TreesitterContextBottom", { sp = "Grey", underline = true })
