99d16ef308471615023bba02b64a9f5d8b04bbe2
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-03-11 14:44:04 +0800

    shell: add tmux keymaps

--- files/shell/.zsh/lib/keymaps.zsh
+++ files/shell/.zsh/lib/keymaps.zsh
@@ -14,17 +14,17 @@ zle -N fzf-history-widget
 bindkey '^r' fzf-history-widget
 
 # workaround to call fzf-tmux-new-session function
-# bindkey -s "^t" 'tfn\n'
+ bindkey -s '^t' 'tfn\n'
 # workaround to call fzf-tmux-attach function
-# bindkey -s "^a" 'tfa\n'
+ bindkey -s '^a' 'tfa\n'
 
 # https://github.com/spaceship-prompt/spaceship-prompt/issues/91#issuecomment-327996599
-bindkey "^?" backward-delete-char
+bindkey '^?' backward-delete-char
 
 bindkey -M menuselect '^[[Z' reverse-menu-complete
 
-zle -N my-clear-screen
-bindkey "^l" my-clear-screen
+zle -N clear-screen-widget
+bindkey '^l' clear-screen-widget
 
 # https://github.com/zsh-users/zsh-history-substring-search#usage
 bindkey '^[[A' history-substring-search-up
