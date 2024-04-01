c1e6da7e138f79452e475193d9a42fdebd63add0
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-04-01 09:33:27 +0800

    nvim: remove unnecessary treesitter_context config

--- files/config/.config/nvim/lua/plugins/treesitter_context.lua
+++ files/config/.config/nvim/lua/plugins/treesitter_context.lua
@@ -3,8 +3,4 @@ return {
   opts = {
     max_lines = 6,
   },
-  config = function(_, opts)
-    vim.api.nvim_set_hl(0, "TreesitterContextBottom", { sp = "Grey", underline = true })
-    require("treesitter-context").setup(opts)
-  end,
 }
