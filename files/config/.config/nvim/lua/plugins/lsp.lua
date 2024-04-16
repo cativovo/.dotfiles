23297e11e78847fd5147b060bfc976a47d3d83ac
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-04-16 20:54:14 +0800

    nvim(lsp): add goto split keybindings

--- files/config/.config/nvim/lua/plugins/lsp.lua
+++ files/config/.config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,22 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<leader>cR", "<cmd>LspRestart<cr>" }
+    keys[#keys + 1] = {
+      "gSv",
+      function()
+        require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
+      end,
+      desc = "Goto Definition (vsplit)",
+      has = "definition",
+    }
+    keys[#keys + 1] = {
+      "gSx",
+      function()
+        require("telescope.builtin").lsp_definitions({ jump_type = "split" })
+      end,
+      desc = "Goto Definition (split)",
+      has = "definition",
+    }
   end,
   opts = {
     diagnostics = {
