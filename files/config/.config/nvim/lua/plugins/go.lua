2d969f5507ac54bc2d936031b5482c33e2412cd4
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-04-22 11:00:20 +0800

    nvim(go): add linter

---
+++ files/config/.config/nvim/lua/plugins/go.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "golangci-lint" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
+    },
+  },
+}
