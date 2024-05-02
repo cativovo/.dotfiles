7b0b2ac6f4aaf7601381b003b37ab01dd7b84d08
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-03 00:44:37 +0800

    nvim(templ): organize config

--- files/config/.config/nvim/lua/plugins/templ.lua
+++ files/config/.config/nvim/lua/plugins/templ.lua
@@ -40,4 +40,21 @@ return {
       },
     },
   },
+  -- mason config
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "rustywind", "templ", "html-lsp", "htmx-lsp" })
+    end,
+  },
+  -- treesitter config
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "templ",
+      })
+    end,
+  },
 }
