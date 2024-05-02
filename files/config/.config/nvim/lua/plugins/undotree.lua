5e3e20a82fb9905fa1e0f4abedbf2622cea155bb
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-02 11:26:47 +0800

    nvim(undotree): auto-focus on toggle

--- files/config/.config/nvim/lua/plugins/undotree.lua
+++ files/config/.config/nvim/lua/plugins/undotree.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     vim.g.undotree_CustomUndotreeCmd = "topleft vertical 30 new"
     vim.g.undotree_CustomDiffpanelCmd = "botright 10 new"
+    vim.g.undotree_SetFocusWhenToggle = 1
   end,
   keys = {
     { "<leader>U", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undo Tree" },
