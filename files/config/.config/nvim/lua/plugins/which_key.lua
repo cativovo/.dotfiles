4eddd64edaac8ee1b6dc0bb1eadddd3d81658c05
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-19 18:02:03 +0800

    nvim: update gitsigns config

--- files/config/.config/nvim/lua/plugins/which_key.lua
+++ files/config/.config/nvim/lua/plugins/which_key.lua
@@ -1,22 +1,27 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+  event = 'VimEnter',
+  config = function()
+    local wk = require('which-key')
+    wk.setup()
+    local git_keymaps = {
+      name = '[G]it',
+      _ = 'which_key_ignore',
+      h = { name = '[h]unk', _ = 'which_key_ignore' },
+    }
 
-    -- Document existing key chains
-    require('which-key').register({
+    wk.register({
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      ['<leader>g'] = git_keymaps,
     })
     -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
+    wk.register({
+      ['<leader>g'] = git_keymaps,
     }, { mode = 'v' })
   end,
 }
