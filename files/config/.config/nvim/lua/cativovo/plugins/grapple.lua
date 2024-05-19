b6d6da649c710ae8734c7567aee056d9306c1cd0
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-20 01:55:57 +0800

    nvim: add grapple

---
+++ files/config/.config/nvim/lua/cativovo/plugins/grapple.lua
@@ -0,0 +1,13 @@
+return {
+  'cbochs/grapple.nvim',
+  event = 'VeryLazy',
+  config = true,
+  keys = {
+    { '<leader>fa', '<cmd>Grapple tag<cr>', desc = 'Tag a file' },
+    { '<leader>fm', '<cmd>Grapple toggle_tags<cr>', desc = 'Toggle tags menu' },
+    { '<leader>fj', '<cmd>Grapple select index=1<cr>', desc = 'Select first tag' },
+    { '<leader>fk', '<cmd>Grapple select index=2<cr>', desc = 'Select second tag' },
+    { '<leader>fl', '<cmd>Grapple select index=3<cr>', desc = 'Select third tag' },
+    { '<leader>f;', '<cmd>Grapple select index=4<cr>', desc = 'Select fourth tag' },
+  },
+}
