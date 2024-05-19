aadfc0572bdfe793d40ca7ac694cc476860665ae
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-20 02:17:48 +0800

    nvim: update keymaps

--- files/config/.config/nvim/lua/cativovo/plugins/which_key.lua
+++ files/config/.config/nvim/lua/cativovo/plugins/which_key.lua
@@ -5,18 +5,19 @@ return {
     local wk = require('which-key')
     wk.setup()
     local git_keymaps = {
-      name = '[G]it',
+      name = 'git',
       _ = 'which_key_ignore',
-      h = { name = '[h]unk', _ = 'which_key_ignore' },
+      h = { name = 'hunk', _ = 'which_key_ignore' },
     }
 
     wk.register({
-      ['<leader>c'] = { name = '[c]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[d]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[r]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[s]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[w]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[t]oggle', _ = 'which_key_ignore' },
+      ['<leader>c'] = { name = 'code', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = 'document', _ = 'which_key_ignore' },
+      ['<leader>r'] = { name = 'rename', _ = 'which_key_ignore' },
+      ['<leader>s'] = { name = 'search', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = 'workspace', _ = 'which_key_ignore' },
+      ['<leader>t'] = { name = 'toggle', _ = 'which_key_ignore' },
+      ['<leader>f'] = { name = 'file', _ = 'which_key_ignore' },
       ['<leader>g'] = git_keymaps,
     })
     -- visual mode
