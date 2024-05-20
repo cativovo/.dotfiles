ab1875869e9aa4f09c481649d205354db8ca8c16
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-20 11:24:22 +0800

    nvim: add toggling of autoformat

--- files/config/.config/nvim/lua/cativovo/plugins/format.lua
+++ files/config/.config/nvim/lua/cativovo/plugins/format.lua
@@ -1,19 +1,37 @@
+local autoformat = true
+
 return {
   'stevearc/conform.nvim',
   lazy = false,
   keys = {
-    -- {
-    --   '<leader>f',
-    --   function()
-    --     require('conform').format({ async = true, lsp_fallback = true })
-    --   end,
-    --   mode = '',
-    --   desc = '[F]ormat buffer',
-    -- },
+    {
+      '<leader>tf',
+      function()
+        autoformat = not autoformat
+        local label = 'off'
+        if autoformat then
+          label = 'on'
+        end
+        vim.notify('autoformat: ' .. label)
+      end,
+      desc = 'toggle autoformat',
+    },
+    {
+      '<leader>fb',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = '',
+      desc = 'format buffer',
+    },
   },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      if not autoformat then
+        return
+      end
+
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
