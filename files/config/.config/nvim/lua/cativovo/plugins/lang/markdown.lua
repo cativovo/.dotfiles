1f3f7f61c7eeefe62d704d8e5f4e3f69d7639191
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-21 21:21:58 +0800

    nvim(lang): add markdown

---
+++ files/config/.config/nvim/lua/cativovo/plugins/lang/markdown.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'markdown', 'markdown_inline' })
+    end,
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'markdownlint', 'marksman' })
+    end,
+  },
+  {
+    'mfussenegger/nvim-lint',
+    opts = {
+      linters_by_ft = {
+        markdown = { 'markdownlint' },
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
+  -- Markdown preview
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+      {
+        '<leader>cp',
+        ft = 'markdown',
+        '<cmd>MarkdownPreviewToggle<cr>',
+        desc = 'Markdown Preview',
+      },
+    },
+    config = function()
+      vim.g.mkdp_auto_close = 0
+      vim.cmd([[do FileType]])
+    end,
+  },
+}
