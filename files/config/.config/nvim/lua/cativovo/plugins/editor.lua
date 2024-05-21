4c2dba4db834770b34769486aa244ebec0f77a44
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-21 20:50:52 +0800

    nvim: add context comment string

--- files/config/.config/nvim/lua/cativovo/plugins/editor.lua
+++ files/config/.config/nvim/lua/cativovo/plugins/editor.lua
@@ -24,7 +24,22 @@ return {
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', config = true },
+  -- { 'numToStr/Comment.nvim', config = true },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+    init = function()
+      vim.schedule(function()
+        local get_option = vim.filetype.get_option
+        vim.filetype.get_option = function(filetype, option)
+          return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+        end
+      end)
+    end,
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
