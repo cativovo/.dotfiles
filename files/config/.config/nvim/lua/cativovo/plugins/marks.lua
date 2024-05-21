fabd381553e8d98d8cec84f7218a1825643f0475
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-22 01:44:17 +0800

    nvim: add marks plugin

---
+++ files/config/.config/nvim/lua/cativovo/plugins/marks.lua
@@ -0,0 +1,16 @@
+return {
+  'chentoast/marks.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local marks = require('marks')
+    vim.keymap.set('n', 'm', marks.set, { desc = 'sets a letter mark (will wait for input)' })
+    vim.keymap.set('n', 'dm', marks.delete, { desc = 'delete a letter mark (will wait for input)' })
+    vim.keymap.set('n', 'dm<space>', marks.delete_buf, { desc = 'deletes all marks in current buffer.' })
+    vim.keymap.set('n', '<leader>qm', '<cmd>MarksQFListBuf<cr>', { desc = 'extract marks to quickfix' })
+
+    require('marks').setup({
+      default_mappings = false,
+      refresh_interval = 300,
+    })
+  end,
+}
