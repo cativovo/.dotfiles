d97cc6e370e52af0e4e09f13f5cc56af4cd218fb
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-22 16:21:56 +0800

    nvim(whichkey): fix labels on visual mode

--- files/config/.config/nvim/lua/cativovo/plugins/which_key.lua
+++ files/config/.config/nvim/lua/cativovo/plugins/which_key.lua
@@ -9,6 +9,11 @@ return {
       _ = 'which_key_ignore',
       h = { name = 'hunk', _ = 'which_key_ignore' },
     }
+    local file_keymaps = {
+      name = 'file',
+      _ = 'which_key_ignore',
+      h = { name = 'file', _ = 'which_key_ignore' },
+    }
 
     wk.register({
       ['<leader>c'] = { name = 'code', _ = 'which_key_ignore' },
@@ -16,13 +21,14 @@ return {
       ['<leader>s'] = { name = 'search', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = 'workspace', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = 'toggle', _ = 'which_key_ignore' },
-      ['<leader>f'] = { name = 'file', _ = 'which_key_ignore' },
       ['<leader>q'] = { name = 'quickfixlist', _ = 'which_key_ignore' },
+      ['<leader>f'] = file_keymaps,
       ['<leader>g'] = git_keymaps,
     })
     -- visual mode
     wk.register({
       ['<leader>g'] = git_keymaps,
+      ['<leader>f'] = file_keymaps,
     }, { mode = 'v' })
   end,
 }
