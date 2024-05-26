e078c01a4a6005d513dc805090585a5010336ff9
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-05-26 15:22:27 +0800

    wezterm: add initial config

---
+++ files/home/.config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font_size = 16
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.9
+-- disable ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+config.disable_default_key_bindings = true
+config.keys = {
+	-- copy/paste
+	{
+		key = "c",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+}
+
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+
+return config
