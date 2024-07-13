a55dabbf71370535918d2bf5bfa4adb26c33b6f6
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-13 12:25:50 +0800

    nvim(which-key): update deprecated spec

--- files/home/.config/nvim/lua/cativovo/plugins/which_key.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/which_key.lua
@@ -4,31 +4,31 @@ return {
   config = function()
     local wk = require('which-key')
     wk.setup()
-    local git_keymaps = {
-      name = 'git',
-      _ = 'which_key_ignore',
-      h = { name = 'hunk', _ = 'which_key_ignore' },
+    local git_keymap = {
+      { '<leader>g', group = 'git' },
+      { '<leader>gh', group = 'hunk' },
     }
-    local file_keymaps = {
-      name = 'file',
-      _ = 'which_key_ignore',
-      y = { name = 'copy', _ = 'which_key_ignore' },
+    local file_keymap = {
+      { '<leader>f', group = 'file' },
+      { '<leader>fy', group = 'copy' },
     }
 
-    wk.register({
-      ['<leader>c'] = { name = 'code', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = 'document/debugger', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = 'search', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = 'workspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = 'toggle', _ = 'which_key_ignore' },
-      ['<leader>q'] = { name = 'quickfixlist', _ = 'which_key_ignore' },
-      ['<leader>f'] = file_keymaps,
-      ['<leader>g'] = git_keymaps,
-    })
+    local normal_keymaps = {
+      { '<leader>c', group = 'code' },
+      { '<leader>d', group = 'document/debugger' },
+      { '<leader>s', group = 'search' },
+      { '<leader>w', group = 'workspace' },
+      { '<leader>t', group = 'toggle' },
+      { '<leader>q', group = 'quickfixlist' },
+    }
+
+    normal_keymaps = vim.tbl_extend('force', normal_keymaps, git_keymap, file_keymap)
+
+    wk.add(normal_keymaps)
+
     -- visual mode
-    wk.register({
-      ['<leader>g'] = git_keymaps,
-      ['<leader>f'] = file_keymaps,
-    }, { mode = 'v' })
+    local visual_keymaps = vim.tbl_extend('force', { mode = 'v' }, git_keymap, file_keymap)
+
+    wk.add(visual_keymaps)
   end,
 }
