abed8d52cf535544cb049ddc8908b8bd654484bc
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-27 15:17:14 +0800

    nvim(lang): add rustywind

--- files/home/.config/nvim/lua/cativovo/plugins/formatting/prettier.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/formatting/prettier.lua
@@ -8,25 +8,33 @@ return {
   },
   {
     'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        ['javascript'] = { 'prettier' },
-        ['javascriptreact'] = { 'prettier' },
-        ['typescript'] = { 'prettier' },
-        ['typescriptreact'] = { 'prettier' },
-        ['vue'] = { 'prettier' },
-        ['css'] = { 'prettier' },
-        ['scss'] = { 'prettier' },
-        ['less'] = { 'prettier' },
-        ['html'] = { 'prettier' },
-        ['json'] = { 'prettier' },
-        ['jsonc'] = { 'prettier' },
-        ['yaml'] = { 'prettier' },
-        ['markdown'] = { 'prettier' },
+    opts = function(_, opts)
+      local formatters_by_ft = {
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        vue = { 'prettier' },
+        css = { 'prettier' },
+        scss = { 'prettier' },
+        less = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        jsonc = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
         ['markdown.mdx'] = { 'prettier' },
-        ['graphql'] = { 'prettier' },
-        ['handlebars'] = { 'prettier' },
-      },
-    },
+        graphql = { 'prettier' },
+        handlebars = { 'prettier' },
+      }
+
+      for ft, formatters in pairs(formatters_by_ft) do
+        if opts.formatters_by_ft[ft] == nil then
+          opts.formatters_by_ft[ft] = formatters
+        else
+          vim.list_extend(opts.formatters_by_ft[ft], formatters)
+        end
+      end
+    end,
   },
 }
--- files/home/.config/nvim/lua/cativovo/plugins/lang/tailwind.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lang/tailwind.lua
@@ -45,4 +45,18 @@ return {
       end
     end,
   },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'rustywind' })
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+      opts.formatters_by_ft.vue = opts.formatters_by_ft.vue or {}
+      vim.list_extend(opts.formatters_by_ft.vue, { 'rustywind' })
+    end,
+  },
 }
