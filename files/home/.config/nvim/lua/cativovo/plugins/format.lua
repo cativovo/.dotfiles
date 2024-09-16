8810ab81cc93f35d7208e0859475068cb7efa881
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-16 13:35:21 +0800

    nvim: update conform config

--- files/home/.config/nvim/lua/cativovo/plugins/format.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/format.lua
@@ -32,13 +32,8 @@ return {
         return
       end
 
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = {}
       return {
         timeout_ms = 3000,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
   },
