ca99029dd2c7ea7223e91a159e8e0d5a6ba3aae1
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-11-25 19:29:54 +0800

    shell: remove unused

--- files/shell/.zsh/lib/exports.zsh
+++ files/shell/.zsh/lib/exports.zsh
@@ -4,9 +4,6 @@
 export VISUAL="nvim"
 export EDITOR="$VISUAL"
 
-# add something here if needed
-export PATH="$PATH:$HOME/.atuin/bin"
-
 # change default config path of starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 
