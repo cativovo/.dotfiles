200bbd3a2fcdbf8c0cd1bffd4c0478ca77bbcc3e
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-03-05 03:59:33 +0800

    nvim: change color of treesitter context

--- files/home/.config/nvim/lua/cativovo/plugins/treesitter.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/treesitter.lua
@@ -1,33 +1,36 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = { 'VeryLazy' },
-    opts = {
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      ensure_installed = { 'bash', 'diff', 'html', 'markdown', 'vim', 'vimdoc' },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        event = { 'VeryLazy' },
+        opts = {
+            -- Autoinstall languages that are not installed
+            auto_install = true,
+            ensure_installed = { 'bash', 'diff', 'html', 'markdown', 'vim', 'vimdoc' },
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+        },
+        config = function(_, opts)
+            -- Prefer git instead of curl in order to improve connectivity in some environments
+            require('nvim-treesitter.install').prefer_git = true
+            require('nvim-treesitter.configs').setup(opts)
+        end,
     },
-    config = function(_, opts)
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function(_, opts)
-      vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { link = 'Cursorline' })
-      vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { link = 'Cursorline' })
-      require('treesitter-context').setup(opts)
-    end,
-    opts = {
-      max_lines = 6,
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function(_, opts)
+            vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { bg = '#313244' })
+            vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { bg = '#313244' })
+            require('treesitter-context').setup(opts)
+        end,
+        opts = {
+            max_lines = 6,
+        },
     },
-  },
 }
