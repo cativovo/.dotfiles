b02ed0fd50d6854cb647662254c9d3df2bd26f18
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-03-13 01:24:30 +0800

    wezterm: update font options

--- files/home/.config/wezterm/wezterm.lua
+++ files/home/.config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("JetBrains Mono", { weight = "Book", stretch = "Expanded" })
 config.font_size = 16
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.9
