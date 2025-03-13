f9a432bff8a6fdc571204b3c21aaeb62b0c669f1
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-03-13 23:28:17 +0800

    nvim: add drop and remove treesitter context

---
+++ files/home/.config/nvim/lua/cativovo/plugins/drop.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        'Bekaboo/dropbar.nvim',
+        -- optional, but required for fuzzy finder support
+        dependencies = {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            -- `cond` is a condition used to determine whether this plugin should be
+            -- installed and loaded.
+            cond = function()
+                return vim.fn.executable('make') == 1
+            end,
+        },
+        config = function()
+            local dropbar_api = require('dropbar.api')
+            vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+            vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+            vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+
+            local dropdown_utils = require('dropbar.utils')
+            local dropbar_opts = require('dropbar.configs')
+
+            dropbar_opts.opts.menu.keymaps['o'] = function()
+                local menu = dropdown_utils.menu.get_current()
+                if not menu then
+                    return
+                end
+                local cursor = vim.api.nvim_win_get_cursor(menu.win)
+                local entry = menu.entries[cursor[1]]
+                local component = entry:first_clickable(entry.padding.left + entry.components[1]:bytewidth())
+                if component then
+                    menu:click_on(component, nil, 1, 'l')
+                end
+            end
+        end,
+    },
+}
--- files/home/.config/nvim/lua/cativovo/plugins/lualine.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lualine.lua
@@ -1,103 +1,93 @@
 return {
-  {
-    'nvim-lualine/lualine.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      {
-        'linrongbin16/lsp-progress.nvim',
-        opts = {
-          format = function(messages)
-            if #messages > 0 then
-              return ' LSP:' .. ' ' .. table.concat(messages, ' ')
-            end
+    {
+        'nvim-lualine/lualine.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            {
+                'linrongbin16/lsp-progress.nvim',
+                opts = {
+                    format = function(messages)
+                        if #messages > 0 then
+                            return ' LSP:' .. ' ' .. table.concat(messages, ' ')
+                        end
 
-            local client_names = {}
-            local clients = vim.lsp.get_clients({ bufnr = 0 })
-            for _, client in ipairs(clients) do
-              if client and client.name ~= '' then
-                table.insert(client_names, client.name)
-              end
-            end
+                        local client_names = {}
+                        local clients = vim.lsp.get_clients({ bufnr = 0 })
+                        for _, client in ipairs(clients) do
+                            if client and client.name ~= '' then
+                                table.insert(client_names, client.name)
+                            end
+                        end
 
-            return ' LSP:' .. ' [' .. table.concat(client_names, ',') .. ']'
-          end,
+                        return ' LSP:' .. ' [' .. table.concat(client_names, ',') .. ']'
+                    end,
+                },
+            },
         },
-      },
+        init = function()
+            vim.g.lualine_laststatus = vim.o.laststatus
+            if vim.fn.argc(-1) > 0 then
+                -- set an empty statusline till lualine loads
+                vim.o.statusline = ' '
+            else
+                -- hide the statusline on the starter page
+                vim.o.laststatus = 0
+            end
+        end,
+        config = function()
+            local icons = require('cativovo.config.icons')
+
+            local opts = {
+                options = {
+                    disabled_filetypes = { statusline = { 'lazy' } },
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_c = {
+                        {
+                            'grapple',
+                        },
+                        {
+                            'diagnostics',
+                            symbols = {
+                                error = icons.diagnostics.Error,
+                                warn = icons.diagnostics.Warn,
+                                info = icons.diagnostics.Info,
+                                hint = icons.diagnostics.Hint,
+                            },
+                        },
+                    },
+                    lualine_x = {
+                        'filetype',
+                        function()
+                            return require('lsp-progress').progress()
+                        end,
+                    },
+                    lualine_y = {
+                        { 'progress' },
+                        { 'location' },
+                    },
+                    lualine_z = {
+                        function()
+                            return 'Lines: ' .. vim.fn.line('$')
+                        end,
+                    },
+                },
+                extensions = {
+                    'lazy',
+                    'quickfix',
+                },
+            }
+
+            require('lualine').setup(opts)
+
+            -- listen lsp-progress event and refresh lualine
+            vim.api.nvim_create_augroup('lualine_augroup', { clear = true })
+            vim.api.nvim_create_autocmd('User', {
+                group = 'lualine_augroup',
+                pattern = 'LspProgressStatusUpdated',
+                callback = require('lualine').refresh,
+            })
+        end,
     },
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        -- set an empty statusline till lualine loads
-        vim.o.statusline = ' '
-      else
-        -- hide the statusline on the starter page
-        vim.o.laststatus = 0
-      end
-    end,
-    config = function()
-      local icons = require('cativovo.config.icons')
-
-      local opts = {
-        options = {
-          disabled_filetypes = { statusline = { 'lazy' } },
-          globalstatus = true,
-        },
-        sections = {
-          lualine_c = {
-            {
-              'grapple',
-            },
-            {
-              'diagnostics',
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
-          },
-          lualine_x = {
-            'filetype',
-            function()
-              return require('lsp-progress').progress()
-            end,
-          },
-          lualine_y = {
-            { 'progress' },
-            { 'location' },
-          },
-          lualine_z = {
-            function()
-              return 'Lines: ' .. vim.fn.line('$')
-            end,
-          },
-        },
-        winbar = {
-          lualine_c = {
-            { 'filename', path = 3, symbols = { modified = ' ' } },
-          },
-        },
-        inactive_winbar = {
-          lualine_c = {
-            { 'filename', path = 3, symbols = { modified = ' ' } },
-          },
-        },
-        extensions = {
-          'lazy',
-          'quickfix',
-        },
-      }
-
-      require('lualine').setup(opts)
-
-      -- listen lsp-progress event and refresh lualine
-      vim.api.nvim_create_augroup('lualine_augroup', { clear = true })
-      vim.api.nvim_create_autocmd('User', {
-        group = 'lualine_augroup',
-        pattern = 'LspProgressStatusUpdated',
-        callback = require('lualine').refresh,
-      })
-    end,
-  },
 }
