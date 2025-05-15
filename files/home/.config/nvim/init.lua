3615ec94dd4617a27670b23a9705c296a0fb8cbc
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-15 21:33:39 +0800

    nvim(lazy): update config

--- files/home/.config/nvim/init.lua
+++ files/home/.config/nvim/init.lua
@@ -4,17 +4,17 @@ require('cativovo.config.autocmds')
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end
 
 ---@type vim.Option
- vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 --
@@ -28,33 +28,10 @@ end
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  spec = {
-    { import = 'cativovo.plugins' },
-    { import = 'cativovo.plugins.lang' },
-    { import = 'cativovo.plugins.linting' },
-    { import = 'cativovo.plugins.formatting' },
-  },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+    spec = {
+        { import = 'cativovo.plugins.lang' },
+        { import = 'cativovo.plugins.linting' },
+        { import = 'cativovo.plugins.formatting' },
+        { import = 'cativovo.plugins' },
     },
-  },
-})
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+}, {})
