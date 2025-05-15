4d2eaa607e7382d21f5a49ee2ccd26621249b903
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-15 15:59:48 +0800

    nvim(telescope): fix border issue and move autocmd to telescope config
    https://github.com/nvim-telescope/telescope.nvim/issues/3436

--- files/home/.config/nvim/lua/cativovo/config/autocmds.lua
+++ files/home/.config/nvim/lua/cativovo/config/autocmds.lua
@@ -1,33 +1,28 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Save Folds
 vim.api.nvim_create_autocmd('BufWinLeave', {
-  pattern = '?*',
-  command = 'silent! mkview',
+    pattern = '?*',
+    command = 'silent! mkview',
 })
 -- Load folds
 vim.api.nvim_create_autocmd('BufReadPost', {
-  pattern = '?*',
-  command = 'silent! loadview',
-})
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'TelescopePreviewerLoaded',
-  command = 'setlocal number',
+    pattern = '?*',
+    command = 'silent! loadview',
 })
 
 vim.api.nvim_create_autocmd('FocusLost', {
-  pattern = '*',
-  command = 'setlocal nornu',
+    pattern = '*',
+    command = 'setlocal nornu',
 })
 
 vim.api.nvim_create_autocmd('FocusGained', {
-  pattern = '*',
-  command = 'setlocal rnu',
+    pattern = '*',
+    command = 'setlocal rnu',
 })
--- files/home/.config/nvim/lua/cativovo/plugins/telescope.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/telescope.lua
@@ -1,135 +1,153 @@
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'TelescopeFindPre',
+    callback = function()
+        vim.opt_local.winborder = 'none'
+        vim.api.nvim_create_autocmd('WinLeave', {
+            once = true,
+            callback = function()
+                vim.opt_local.winborder = 'rounded'
+            end,
+        })
+    end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'TelescopePreviewerLoaded',
+    command = 'setlocal number',
+})
+
 local grep_opts = {
-  additional_args = {
-    '--hidden',
-  },
+    additional_args = {
+        '--hidden',
+    },
 }
 
 -- for lsp_symbols pickers
 local symbol_width = 60
 
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    -- If encountering errors, see telescope-fzf-native README for installation instructions
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
-      build = 'make',
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
-      cond = function()
-        return vim.fn.executable('make') == 1
-      end,
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- If encountering errors, see telescope-fzf-native README for installation instructions
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            -- `build` is used to run some command when the plugin is installed/updated.
+            -- This is only run then, not every time Neovim starts up.
+            build = 'make',
+            -- `cond` is a condition used to determine whether this plugin should be
+            -- installed and loaded.
+            cond = function()
+                return vim.fn.executable('make') == 1
+            end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+
+        -- Useful for getting pretty icons, but requires a Nerd Font.
+        { 'nvim-tree/nvim-web-devicons' },
     },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-
-    -- Useful for getting pretty icons, but requires a Nerd Font.
-    { 'nvim-tree/nvim-web-devicons' },
-  },
-  keys = function()
-    local builtin = require('telescope.builtin')
-    return {
-      { '<leader>sh', builtin.help_tags, desc = 'search help' },
-      { '<leader>sk', builtin.keymaps, desc = 'search keymaps' },
-      { '<leader>sf', builtin.find_files, desc = 'search files' },
-      { '<leader>sb', builtin.builtin, desc = 'search builtin telescope' },
-      { '<leader>sw', builtin.grep_string, desc = 'search current word' },
-      { '<leader>sg', builtin.live_grep, desc = 'search by grep' },
-      { '<leader>sr', builtin.resume, desc = 'search resume' },
-      { '<leader>sS', builtin.spell_suggest, desc = 'search spelling' },
-      { '<leader>sm', builtin.marks, desc = 'search marks' },
-      { '<leader><leader>', builtin.buffers, desc = 'find existing buffers' },
-      {
-        '<leader>s.',
-        function()
-          builtin.oldfiles({ cwd_only = true })
-        end,
-        desc = 'search recent files',
-      },
-      {
-        '<leader>/',
-        function()
-          -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-          builtin.current_buffer_fuzzy_find({
-            layout_config = {
-              vertical = {
-                preview_height = 0.40,
-              },
+    keys = function()
+        local builtin = require('telescope.builtin')
+        return {
+            { '<leader>sh', builtin.help_tags, desc = 'search help' },
+            { '<leader>sk', builtin.keymaps, desc = 'search keymaps' },
+            { '<leader>sf', builtin.find_files, desc = 'search files' },
+            { '<leader>sb', builtin.builtin, desc = 'search builtin telescope' },
+            { '<leader>sw', builtin.grep_string, desc = 'search current word' },
+            { '<leader>sg', builtin.live_grep, desc = 'search by grep' },
+            { '<leader>sr', builtin.resume, desc = 'search resume' },
+            { '<leader>sS', builtin.spell_suggest, desc = 'search spelling' },
+            { '<leader>sm', builtin.marks, desc = 'search marks' },
+            { '<leader><leader>', builtin.buffers, desc = 'find existing buffers' },
+            {
+                '<leader>s.',
+                function()
+                    builtin.oldfiles({ cwd_only = true })
+                end,
+                desc = 'search recent files',
             },
-          })
-        end,
-        desc = 'fuzzily search in current buffer',
-      },
-      {
-        '<leader>s/',
-        function()
-          builtin.live_grep({
-            grep_open_files = true,
-            prompt_title = 'live Grep in Open Files',
-          })
-        end,
-        desc = 'search in open files',
-      },
-    }
-  end,
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        file_ignore_patterns = { '.git/' },
-        sorting_strategy = 'ascending',
-        layout_strategy = 'vertical',
-        layout_config = {
-          vertical = {
-            preview_cutoff = 0,
-            prompt_position = 'top',
-            mirror = true,
-            preview_height = 0.60,
-          },
-          width = 0.90,
-          height = 0.99,
-        },
-      },
-      pickers = {
-        git_status = {
-          previewer = false,
-          theme = 'dropdown',
-        },
-        live_grep = grep_opts,
-        grep_string = grep_opts,
-        find_files = {
-          hidden = true,
-        },
-        lsp_references = {
-          show_line = false,
-        },
-        lsp_document_symbols = {
-          symbol_width = symbol_width,
-        },
-        lsp_dynamic_workspace_symbols = {
-          symbol_width = symbol_width,
-        },
-        buffers = {
-          mappings = {
-            i = {
-              ['<C-c>'] = function(prompt_bufnr)
-                require('telescope.actions').delete_buffer(prompt_bufnr)
-              end,
+            {
+                '<leader>/',
+                function()
+                    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                    builtin.current_buffer_fuzzy_find({
+                        layout_config = {
+                            vertical = {
+                                preview_height = 0.40,
+                            },
+                        },
+                    })
+                end,
+                desc = 'fuzzily search in current buffer',
             },
-          },
-        },
-      },
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
-        },
-      },
-    })
+            {
+                '<leader>s/',
+                function()
+                    builtin.live_grep({
+                        grep_open_files = true,
+                        prompt_title = 'live Grep in Open Files',
+                    })
+                end,
+                desc = 'search in open files',
+            },
+        }
+    end,
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = { '.git/' },
+                sorting_strategy = 'ascending',
+                layout_strategy = 'vertical',
+                layout_config = {
+                    vertical = {
+                        preview_cutoff = 0,
+                        prompt_position = 'top',
+                        mirror = true,
+                        preview_height = 0.60,
+                    },
+                    width = 0.90,
+                    height = 0.99,
+                },
+            },
+            pickers = {
+                git_status = {
+                    previewer = false,
+                    theme = 'dropdown',
+                },
+                live_grep = grep_opts,
+                grep_string = grep_opts,
+                find_files = {
+                    hidden = true,
+                },
+                lsp_references = {
+                    show_line = false,
+                },
+                lsp_document_symbols = {
+                    symbol_width = symbol_width,
+                },
+                lsp_dynamic_workspace_symbols = {
+                    symbol_width = symbol_width,
+                },
+                buffers = {
+                    mappings = {
+                        i = {
+                            ['<C-c>'] = function(prompt_bufnr)
+                                require('telescope.actions').delete_buffer(prompt_bufnr)
+                            end,
+                        },
+                    },
+                },
+            },
+            extensions = {
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown(),
+                },
+            },
+        })
 
-    -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
-  end,
+        -- Enable Telescope extensions if they are installed
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+    end,
 }
