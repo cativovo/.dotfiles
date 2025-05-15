bd57690341428655fee6aa3a9e4aadf46972f204
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-15 16:02:13 +0800

    nvim(typescript): remove dap config

--- files/home/.config/nvim/lua/cativovo/plugins/lang/_typescript.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lang/_typescript.lua
@@ -1,236 +1,172 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == 'table' then
-        vim.list_extend(opts.ensure_installed, { 'typescript', 'tsx' })
-      end
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        vtsls = {
-          -- explicitly add default filetypes, so that we can extend
-          -- them in related extras
-          filetypes = {
-            'javascript',
-            'javascriptreact',
-            'javascript.jsx',
-            'typescript',
-            'typescriptreact',
-            'typescript.tsx',
-            'vue',
-          },
-          settings = {
-            complete_function_calls = true,
-            vtsls = {
-              enableMoveToFileCodeAction = true,
-              autoUseWorkspaceTsdk = true,
-              experimental = {
-                completion = {
-                  enableServerSideFuzzyMatch = true,
-                },
-              },
-            },
-            typescript = {
-              updateImportsOnFileMove = { enabled = 'always' },
-              suggest = {
-                completeFunctionCalls = true,
-              },
-              inlayHints = {
-                enumMemberValues = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                parameterNames = { enabled = 'literals' },
-                parameterTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                variableTypes = { enabled = false },
-              },
-            },
-          },
-          keys = {
-            {
-              'gD',
-              function()
-                require('vtsls').commands.goto_source_definition(0)
-              end,
-              desc = 'Goto Source Definition',
-            },
-            {
-              'gR',
-              function()
-                require('vtsls').commands.file_references(0)
-              end,
-              desc = 'File References',
-            },
-            {
-              '<leader>co',
-              function()
-                require('vtsls').commands.organize_imports(0)
-              end,
-              desc = 'Organize Imports',
-            },
-            {
-              '<leader>cM',
-              function()
-                require('vtsls').commands.add_missing_imports(0)
-              end,
-              desc = 'Add missing imports',
-            },
-            {
-              '<leader>cu',
-              function()
-                require('vtsls').commands.remove_unused_imports(0)
-              end,
-              desc = 'Remove unused imports',
-            },
-            {
-              '<leader>cD',
-              function()
-                require('vtsls').commands.fix_all(0)
-              end,
-              desc = 'Fix all diagnostics',
-            },
-            {
-              '<leader>cV',
-              function()
-                require('vtsls').commands.select_ts_version(0)
-              end,
-              desc = 'Select TS workspace version',
-            },
-          },
-        },
-      },
-      setups = {
-        vtsls = function(opts)
-          require('cativovo.utils').on_lsp_attach(function(client, buffer)
-            client.commands['_typescript.moveToFileRefactoring'] = function(command, ctx)
-              ---@type string, string, lsp.Range
-              local action, uri, range = unpack(command.arguments)
-
-              local function move(newf)
-                client.request('workspace/executeCommand', {
-                  command = command.command,
-                  arguments = { action, uri, range, newf },
-                })
-              end
-
-              local fname = vim.uri_to_fname(uri)
-              client.request('workspace/executeCommand', {
-                command = 'typescript.tsserverRequest',
-                arguments = {
-                  'getMoveToRefactoringFileSuggestions',
-                  {
-                    file = fname,
-                    startLine = range.start.line + 1,
-                    startOffset = range.start.character + 1,
-                    endLine = range['end'].line + 1,
-                    endOffset = range['end'].character + 1,
-                  },
-                },
-              }, function(_, result)
-                ---@type string[]
-                local files = result.body.files
-                table.insert(files, 1, 'Enter new path...')
-                vim.ui.select(files, {
-                  prompt = 'Select move destination:',
-                  format_item = function(f)
-                    return vim.fn.fnamemodify(f, ':~:.')
-                  end,
-                }, function(f)
-                  if f and f:find('^Enter new path') then
-                    vim.ui.input({
-                      prompt = 'Enter move destination:',
-                      default = vim.fn.fnamemodify(fname, ':h') .. '/',
-                      completion = 'file',
-                    }, function(newf)
-                      return newf and move(newf)
-                    end)
-                  elseif f then
-                    move(f)
-                  end
-                end)
-              end)
-            end
-          end, 'vtsls')
-          -- copy typescript settings to javascript
-          opts.settings.javascript = vim.tbl_deep_extend('force', {}, opts.settings.typescript, opts.settings.javascript or {})
-        end,
-      },
-    },
-  },
-  {
-    'yioneko/nvim-vtsls',
-    lazy = true,
-    opts = {},
-    config = function(_, opts)
-      require('vtsls').config(opts)
-    end,
-  },
-  {
-    'mfussenegger/nvim-dap',
-    dependencies = {
-      {
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
+    {
+        'nvim-treesitter/nvim-treesitter',
         opts = function(_, opts)
-          opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { 'js-debug-adapter' })
+            if type(opts.ensure_installed) == 'table' then
+                vim.list_extend(opts.ensure_installed, { 'typescript', 'tsx' })
+            end
         end,
-      },
     },
-    opts = function()
-      local dap = require('dap')
-      if not dap.adapters['pwa-node'] then
-        require('dap').adapters['pwa-node'] = {
-          type = 'server',
-          host = 'localhost',
-          port = '${port}',
-          executable = {
-            command = 'node',
-            args = {
-              require('mason-registry').get_package('js-debug-adapter'):get_install_path() .. '/js-debug/src/dapDebugServer.js',
-              '${port}',
+    {
+        'neovim/nvim-lspconfig',
+        opts = {
+            -- make sure mason installs the server
+            servers = {
+                vtsls = {
+                    -- explicitly add default filetypes, so that we can extend
+                    -- them in related extras
+                    filetypes = {
+                        'javascript',
+                        'javascriptreact',
+                        'javascript.jsx',
+                        'typescript',
+                        'typescriptreact',
+                        'typescript.tsx',
+                        'vue',
+                    },
+                    settings = {
+                        complete_function_calls = true,
+                        vtsls = {
+                            enableMoveToFileCodeAction = true,
+                            autoUseWorkspaceTsdk = true,
+                            experimental = {
+                                completion = {
+                                    enableServerSideFuzzyMatch = true,
+                                },
+                            },
+                        },
+                        typescript = {
+                            updateImportsOnFileMove = { enabled = 'always' },
+                            suggest = {
+                                completeFunctionCalls = true,
+                            },
+                            inlayHints = {
+                                enumMemberValues = { enabled = true },
+                                functionLikeReturnTypes = { enabled = true },
+                                parameterNames = { enabled = 'literals' },
+                                parameterTypes = { enabled = true },
+                                propertyDeclarationTypes = { enabled = true },
+                                variableTypes = { enabled = false },
+                            },
+                        },
+                    },
+                    keys = {
+                        {
+                            'gD',
+                            function()
+                                require('vtsls').commands.goto_source_definition(0)
+                            end,
+                            desc = 'Goto Source Definition',
+                        },
+                        {
+                            'gR',
+                            function()
+                                require('vtsls').commands.file_references(0)
+                            end,
+                            desc = 'File References',
+                        },
+                        {
+                            '<leader>co',
+                            function()
+                                require('vtsls').commands.organize_imports(0)
+                            end,
+                            desc = 'Organize Imports',
+                        },
+                        {
+                            '<leader>cM',
+                            function()
+                                require('vtsls').commands.add_missing_imports(0)
+                            end,
+                            desc = 'Add missing imports',
+                        },
+                        {
+                            '<leader>cu',
+                            function()
+                                require('vtsls').commands.remove_unused_imports(0)
+                            end,
+                            desc = 'Remove unused imports',
+                        },
+                        {
+                            '<leader>cD',
+                            function()
+                                require('vtsls').commands.fix_all(0)
+                            end,
+                            desc = 'Fix all diagnostics',
+                        },
+                        {
+                            '<leader>cV',
+                            function()
+                                require('vtsls').commands.select_ts_version(0)
+                            end,
+                            desc = 'Select TS workspace version',
+                        },
+                    },
+                },
             },
-          },
-        }
-      end
+            setups = {
+                vtsls = function(opts)
+                    require('cativovo.utils').on_lsp_attach(function(client, buffer)
+                        client.commands['_typescript.moveToFileRefactoring'] = function(command, ctx)
+                            ---@type string, string, lsp.Range
+                            local action, uri, range = unpack(command.arguments)
 
-      if not dap.adapters['node'] then
-        dap.adapters['node'] = function(cb, config)
-          if config.type == 'node' then
-            config.type = 'pwa-node'
-          end
-          local nativeAdapter = dap.adapters['pwa-node']
-          if type(nativeAdapter) == 'function' then
-            nativeAdapter(cb, config)
-          else
-            cb(nativeAdapter)
-          end
-        end
-      end
+                            local function move(newf)
+                                client.request('workspace/executeCommand', {
+                                    command = command.command,
+                                    arguments = { action, uri, range, newf },
+                                })
+                            end
 
-      for _, language in ipairs({ 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }) do
-        if not dap.configurations[language] then
-          dap.configurations[language] = {
-            {
-              type = 'pwa-node',
-              request = 'launch',
-              name = 'Launch file',
-              program = '${file}',
-              cwd = '${workspaceFolder}',
+                            local fname = vim.uri_to_fname(uri)
+                            client.request('workspace/executeCommand', {
+                                command = 'typescript.tsserverRequest',
+                                arguments = {
+                                    'getMoveToRefactoringFileSuggestions',
+                                    {
+                                        file = fname,
+                                        startLine = range.start.line + 1,
+                                        startOffset = range.start.character + 1,
+                                        endLine = range['end'].line + 1,
+                                        endOffset = range['end'].character + 1,
+                                    },
+                                },
+                            }, function(_, result)
+                                ---@type string[]
+                                local files = result.body.files
+                                table.insert(files, 1, 'Enter new path...')
+                                vim.ui.select(files, {
+                                    prompt = 'Select move destination:',
+                                    format_item = function(f)
+                                        return vim.fn.fnamemodify(f, ':~:.')
+                                    end,
+                                }, function(f)
+                                    if f and f:find('^Enter new path') then
+                                        vim.ui.input({
+                                            prompt = 'Enter move destination:',
+                                            default = vim.fn.fnamemodify(fname, ':h') .. '/',
+                                            completion = 'file',
+                                        }, function(newf)
+                                            return newf and move(newf)
+                                        end)
+                                    elseif f then
+                                        move(f)
+                                    end
+                                end)
+                            end)
+                        end
+                    end, 'vtsls')
+                    -- copy typescript settings to javascript
+                    opts.settings.javascript = vim.tbl_deep_extend('force', {}, opts.settings.typescript, opts.settings.javascript or {})
+                end,
             },
-            {
-              type = 'pwa-node',
-              request = 'attach',
-              name = 'Node attach',
-              processId = require('dap.utils').pick_process,
-              cwd = '${workspaceFolder}',
-            },
-          }
-        end
-      end
-    end,
-  },
+        },
+    },
+    {
+        'yioneko/nvim-vtsls',
+        lazy = true,
+        opts = {},
+        config = function(_, opts)
+            require('vtsls').config(opts)
+        end,
+    },
 }
