f2e0eaa968773804d7ebaaabf82894d3bf79db26
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-18 18:03:36 +0800

    nvim(lualine): simplify config

--- files/home/.config/nvim/lua/cativovo/plugins/lualine.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lualine.lua
@@ -1,93 +1,51 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        event = { 'BufReadPre', 'BufNewFile' },
-        dependencies = {
-            {
-                'linrongbin16/lsp-progress.nvim',
-                opts = {
-                    format = function(messages)
-                        if #messages > 0 then
-                            return ' LSP:' .. ' ' .. table.concat(messages, ' ')
-                        end
-
-                        local client_names = {}
-                        local clients = vim.lsp.get_clients({ bufnr = 0 })
-                        for _, client in ipairs(clients) do
-                            if client and client.name ~= '' then
-                                table.insert(client_names, client.name)
-                            end
-                        end
-
-                        return ' LSP:' .. ' [' .. table.concat(client_names, ',') .. ']'
+    'nvim-lualine/lualine.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            -- set an empty statusline till lualine loads
+            vim.o.statusline = ' '
+        else
+            -- hide the statusline on the starter page
+            vim.o.laststatus = 0
+        end
+    end,
+    opts = {
+        options = {
+            disabled_filetypes = { statusline = { 'lazy' } },
+            globalstatus = true,
+        },
+        sections = {
+            lualine_a = { 'branch' },
+            lualine_b = { 'diagnostics' },
+            lualine_c = {
+                {
+                    'filename',
+                    path = 1,
+                },
+                'grapple',
+            },
+            lualine_x = {
+                'filetype',
+                'lsp_status',
+            },
+            lualine_y = {
+                {
+                    -- https://github.com/nvim-lualine/lualine.nvim/blob/15884cee63a8c205334ab13ab1c891cd4d27101a/lua/lualine/components/location.lua#L1
+                    function()
+                        local line = vim.fn.line('.')
+                        local col = vim.fn.charcol('.')
+                        local total = vim.fn.line('$')
+                        return string.format('%d/%d:%d', line, total, col)
                     end,
                 },
             },
+            lualine_z = {},
         },
-        init = function()
-            vim.g.lualine_laststatus = vim.o.laststatus
-            if vim.fn.argc(-1) > 0 then
-                -- set an empty statusline till lualine loads
-                vim.o.statusline = ' '
-            else
-                -- hide the statusline on the starter page
-                vim.o.laststatus = 0
-            end
-        end,
-        config = function()
-            local icons = require('cativovo.config.icons')
-
-            local opts = {
-                options = {
-                    disabled_filetypes = { statusline = { 'lazy' } },
-                    globalstatus = true,
-                },
-                sections = {
-                    lualine_c = {
-                        {
-                            'grapple',
-                        },
-                        {
-                            'diagnostics',
-                            symbols = {
-                                error = icons.diagnostics.Error,
-                                warn = icons.diagnostics.Warn,
-                                info = icons.diagnostics.Info,
-                                hint = icons.diagnostics.Hint,
-                            },
-                        },
-                    },
-                    lualine_x = {
-                        'filetype',
-                        function()
-                            return require('lsp-progress').progress()
-                        end,
-                    },
-                    lualine_y = {
-                        { 'progress' },
-                        { 'location' },
-                    },
-                    lualine_z = {
-                        function()
-                            return 'Lines: ' .. vim.fn.line('$')
-                        end,
-                    },
-                },
-                extensions = {
-                    'lazy',
-                    'quickfix',
-                },
-            }
-
-            require('lualine').setup(opts)
-
-            -- listen lsp-progress event and refresh lualine
-            vim.api.nvim_create_augroup('lualine_augroup', { clear = true })
-            vim.api.nvim_create_autocmd('User', {
-                group = 'lualine_augroup',
-                pattern = 'LspProgressStatusUpdated',
-                callback = require('lualine').refresh,
-            })
-        end,
+        extensions = {
+            'lazy',
+            'quickfix',
+        },
     },
 }
