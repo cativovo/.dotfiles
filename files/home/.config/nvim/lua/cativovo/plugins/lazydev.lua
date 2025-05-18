d4a94d9cb464352ae33fd382e5437e1854c12993
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-18 20:49:22 +0800

    nvim(lazydev): update config

--- files/home/.config/nvim/lua/cativovo/plugins/lazydev.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lazydev.lua
@@ -1,24 +1,12 @@
 return {
-  'folke/lazydev.nvim',
-  ft = 'lua', -- only load on lua files
-  opts = {
-    library = {
-      -- See the configuration section for more details
-      -- Load luvit types when the `vim.uv` word is found
-      { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+        library = {
+            -- Load luvit types when the `vim.uv` word is found
+            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        },
     },
-  },
-  dependencies = {
-    { 'Bilal2453/luvit-meta' }, -- optional `vim.uv` typings
-    { -- optional completion source for require statements and module annotations
-      'hrsh7th/nvim-cmp',
-      opts = function(_, opts)
-        opts.sources = opts.sources or {}
-        table.insert(opts.sources, {
-          name = 'lazydev',
-          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-        })
-      end,
-    },
-  },
 }
