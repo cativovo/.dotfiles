fe22f6ddacfc96353191bb7ed306adb158a0bb22
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-05-18 23:16:38 +0800

    nvim: replace cmp with blink

---
+++ files/home/.config/nvim/lua/cativovo/plugins/blink.lua
@@ -0,0 +1,51 @@
+return {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+        {
+            'L3MON4D3/LuaSnip',
+            version = '2.*',
+            build = (function()
+                -- Build Step is needed for regex support in snippets.
+                -- This step is not supported in many windows environments.
+                -- Remove the below condition to re-enable on windows.
+                if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
+                    return
+                end
+                return 'make install_jsregexp'
+            end)(),
+            dependencies = {
+                {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end,
+                },
+            },
+            opts = {},
+        },
+        'folke/lazydev.nvim',
+    },
+    opts = {
+        keymap = {
+            preset = 'default',
+        },
+        appearance = {
+            nerd_font_variant = 'mono',
+        },
+        completion = {
+            documentation = { auto_show = true, auto_show_delay_ms = 100 },
+            ghost_text = { enabled = true, show_with_menu = true },
+        },
+        sources = {
+            default = { 'lsp', 'snippets', 'path', 'lazydev' },
+            providers = {
+                lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+            },
+        },
+        snippets = { preset = 'luasnip' },
+        fuzzy = { implementation = 'lua' },
+        signature = { enabled = true },
+    },
+}
