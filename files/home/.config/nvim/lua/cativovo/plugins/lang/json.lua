e4fa8959d2a72ed000ec5a93762a39714bcb1ff1
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-06-01 18:51:59 +0800

    nvim(json): auto install jsonlint

--- files/home/.config/nvim/lua/cativovo/plugins/lang/json.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/lang/json.lua
@@ -3,7 +3,14 @@ return {
         'nvim-treesitter/nvim-treesitter',
         opts = function(_, opts)
             opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { 'json', 'json5', 'jsonc', 'jsonlint' })
+            vim.list_extend(opts.ensure_installed, { 'json', 'json5', 'jsonc' })
+        end,
+    },
+    {
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            vim.list_extend(opts.ensure_installed, { 'jsonlint' })
         end,
     },
     {
