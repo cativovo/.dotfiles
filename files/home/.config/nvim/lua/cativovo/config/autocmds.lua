0f2ea60bb468e8b1d85a0ec15cd6a9ee3794cd3f
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-06-19 22:04:07 +0800

    nvim: group autocmds

--- files/home/.config/nvim/lua/cativovo/config/autocmds.lua
+++ files/home/.config/nvim/lua/cativovo/config/autocmds.lua
@@ -6,23 +6,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+local folds = vim.api.nvim_create_augroup('folds', { clear = true })
 -- Save Folds
 vim.api.nvim_create_autocmd('BufWinLeave', {
+    group = folds,
     pattern = '?*',
     command = 'silent! mkview',
 })
 -- Load folds
 vim.api.nvim_create_autocmd('BufReadPost', {
+    group = folds,
     pattern = '?*',
     command = 'silent! loadview',
 })
 
+local focus = vim.api.nvim_create_augroup('focus', { clear = true })
 vim.api.nvim_create_autocmd('FocusLost', {
+    group = focus,
     pattern = '*',
     command = 'setlocal nornu',
 })
-
 vim.api.nvim_create_autocmd('FocusGained', {
+    group = focus,
     pattern = '*',
     command = 'setlocal rnu',
 })
