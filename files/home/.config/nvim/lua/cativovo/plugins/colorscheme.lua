fd81b122e17fb197ea9178a14176def8707fab8c
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-08-08 03:34:41 +0800

    nvim(colorscheme): pin catpuccin version

--- files/home/.config/nvim/lua/cativovo/plugins/colorscheme.lua
+++ files/home/.config/nvim/lua/cativovo/plugins/colorscheme.lua
@@ -1,23 +1,36 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  opts = {
-    flavour = 'mocha', -- latte, frappe, macchiato, mocha
-    background = {
-      light = 'latte',
-      dark = 'mocha',
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    commit = '931a129463ca09c8805d564a28b3d0090e536e1d',
+    opts = {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        background = {
+            light = 'latte',
+            dark = 'mocha',
+        },
+        float = {
+            transparent = true,
+        },
+        transparent_background = true,
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            telescope = true,
+            mason = true,
+            flash = true,
+            fidget = true,
+        },
     },
     transparent_background = true,
     integrations = {
-      cmp = true,
-      gitsigns = true,
-      telescope = true,
-      mason = true,
-      flash = true,
-      fidget = true,
+        cmp = true,
+        gitsigns = true,
+        telescope = true,
+        mason = true,
+        flash = true,
+        fidget = true,
     },
-  },
-  init = function()
-    vim.cmd.colorscheme('catppuccin')
-  end,
+    init = function()
+        vim.cmd.colorscheme('catppuccin')
+    end,
 }
